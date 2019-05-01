02f149776e2253859e065e81f58bc90eee5990ac
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-01 19:31:33 -0400

    R script for downloading texts

---
+++ data_prep/gutenberg.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env R
+
+
+library("gutenbergr")
+library("dplyr")
+library("tidytext")
+library("stringr")
+
+works <- gutenberg_works()
+
+author_works <- function(author_name) {
+  gutenberg_works() %>%
+    filter(author == author_name) %>%
+    .[["gutenberg_id"]]
+}
+
+lines_to_sentences <- function(lines_data) {
+  lines_data$text <- stri_enc_toutf8(lines_data$text, validate=TRUE)
+  unnest_tokens(lines_data, sentence, text, token = "sentences")
+}
+
+## Downloading and reshaping some of the dataset
+ids <- list(
+  "keats" = author_works("Keats, John"),
+  "wordsworth" = author_works("Wordsworth, William")
+)
+
+texts <- list(
+  "keats" = gutenberg_download(ids[["keats"]], verbose=TRUE),
+  "wordsworth" = gutenberg_download(ids[["wordsworth"]])
+)
+
+sentences <- rbind(
+  data.frame(author = "keats", lines_to_sentences(texts[["keats"]])),
+  data.frame(author = "wordsworth", lines_to_sentences(texts[["wordsworth"]]))
+)
+
+## some basic text preprocessing
+sentences$sentence <- gsub("[[:punct:]]+","", sentences$sentence)
+sentences$sentence <- gsub("\\s+", " ", str_trim(sentences$sentence))
+sentences <- sentences %>%
+  filter(!grepl("footnote", sentence)) %>%
+  mutate(n_words = str_count(sentence, " ") + 1) %>%
+  filter(n_words > 5)
+
+write.csv(sentences, file = "sentences.csv", row.names = FALSE)
