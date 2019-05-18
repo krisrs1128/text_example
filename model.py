b64f2ae59d7ce55732ce2843b43f08e1ae0d3a0f
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-17 20:19:02 -0400

    training and model definition

--- model.py
+++ model.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
+import torch.nn as nn
 
-def indexesFromSentence(voc, sentence):
-    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+def train(model, iterator, optimizer, criterion, embedding, device):
+    epoch_loss = 0
+    model.train()
+
+    for elem in iterator:
+        optimizer.zero_grad()
+        x = embedding(elem.sentence)
+        x = x.to(device)
+        y = elem.author.float().to(device)
+
+        _, _, y_hat = model(x)
+
+        loss = criterion(y_hat.squeeze(1), y)
+
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
 
 
-def zeroPadding(l, fillvalue=PAD_token):
-    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
+class RNN(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, output_dim, num_layers, dropout):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.batch_size = 32
+        self.rnn = nn.GRU(
+            embedding_dim,
+            hidden_dim,
+            num_layers=num_layers,
+            dropout=dropout
+        )
 
-def binaryMatrix(l, value=PAD_token):
-    m = []
-    for i, seq in enumerate(l):
-        m.append([])
-        for token in seq:
-            if token == PAD_token:
-                m[i].append(0)
-            else:
-                m[i].append(1)
-    return m
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
 
-# Returns padded input sequence tensor and lengths
-def inputVar(l, voc):
-    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
-    padList = zeroPadding(indexes_batch)
-    padVar = torch.LongTensor(padList)
-    return padVar, lengths
+    def forward(self, x):
+        h_1n, h_n = self.rnn(x)
+        linear_hn = self.fc(h_n[-1, :])
+        return h_1n, h_n, torch.sigmoid(linear_hn)
