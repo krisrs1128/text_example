71aa1c07d611ffbfef72f88330e9958e6e4aa12c
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-28 21:35:03 -0400

    some commentary for the data download

--- data_prep/gutenberg.R
+++ data_prep/gutenberg.R
@@ -1,26 +1,34 @@
 #!/usr/bin/env R
-
-
+#'
+#' Data from Project Gutenberg
+#'
+#' This is an example of using the gutenbergr to download and lightly preprocess
+#' sentences for later use in a classification system. The question here is to
+#' classify between poems by john keats and william wordsworth -- they are both
+#' poets with quite a few poems on Project Gutenberg.
 library("gutenbergr")
 library("dplyr")
 library("tidytext")
 library("stringr")
 library("stringi")
 
-works <- gutenberg_works()
+# Simple wrapper functions
 
+#' Gutenberg IDs for an Author
 author_works <- function(author_name) {
   gutenberg_works() %>%
     filter(author == author_name) %>%
     .[["gutenberg_id"]]
 }
 
+#' Use tidytext to split sentences
 lines_to_sentences <- function(lines_data) {
   lines_data$text <- stri_enc_toutf8(lines_data$text, validate=TRUE)
   unnest_tokens(lines_data, sentence, text, token = "sentences")
 }
 
-## Downloading and reshaping some of the dataset
+#' Downloading and reshaping some of the dataset
+works <- gutenberg_works()
 ids <- list(
   "keats" = author_works("Keats, John"),
   "wordsworth" = author_works("Wordsworth, William")
@@ -36,7 +44,7 @@ sentences <- rbind(
   data.frame(author = "wordsworth", lines_to_sentences(texts[["wordsworth"]]))
 )
 
-## some basic text preprocessing
+#' some basic text preprocessing
 sentences$sentence <- gsub("[[:punct:]]+","", sentences$sentence)
 sentences$sentence <- gsub("\\s+", " ", str_trim(sentences$sentence))
 sentences <- sentences %>%
